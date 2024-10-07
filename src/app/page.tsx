e62fb60edd1be86cdcc9c16721fc24f7c2941e3f
abhishekgusain07 <abhishek.gusain1007fb@gmail.com>
2024-10-07 20:30:59 +0530

    🥂 basic setup

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="h-screen w-full flex flex-col justify-center items-center">
+      <h1 className="text-4xl font-bold">Find My Roommate</h1>
+      <p className="text-2xl text-gray-500">
+        Find My Roommate is a platform that helps you find a roommate for your
+        apartment, pg, house, etc.
+      </p>
+    </div>
+  );
+}
