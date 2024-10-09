c6007bd4af22d6760294bfd46437ca9cc01488b3
abhishekgusain07 <abhishek.gusain1007fb@gmail.com>
2024-10-09 11:19:01 +0530

    ✅ feat: auth

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen w-full gap-[5px]">
+      <h1 className="text-4xl font-bold">Find My Flatmate</h1>
+      <p className="font-semibold text-muted-foreground">By BuildingFullThrotle</p>
+    </div>
+  );
+}
