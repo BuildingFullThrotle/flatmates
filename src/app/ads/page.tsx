773119018071260732ca9d159f24494ec2a8aa9e
LostLord07 <satishsingh45264209@gmail.com>
2024-10-10 21:04:14 +0530

    some modifications in signUp form

---
+++ src/app/ads/page.tsx
@@ -0,0 +1,9 @@
+const AdsPage = () => {
+    return (
+        <div>
+            <h1>Ads Page</h1>
+        </div>
+    )
+}
+
+export default AdsPage
---
+++ src/app/test/page.tsx
@@ -0,0 +1,43 @@
+'use client'
+import { Button } from "@/components/ui/button"
+import { CheckCircle2, Loader2, X } from "lucide-react"
+import { useState } from "react"
+import { toast } from "sonner"
+
+const TestPage = () => {
+    const [testingFlatbuddyBackend, setTestingFlatbuddyBackend] = useState<boolean>(false)
+    const [testingFlatbuddyBackendResult, setTestingFlatbuddyBackendResult] = useState<boolean>(false)
+    const testFlatbuddyBackend = async () => {
+        setTestingFlatbuddyBackend(true)
+        try {
+            const response = await fetch('http://localhost:3000/api/test')
+            const data = await response.json()
+            if(data.status === 'Up and Running') {
+                toast.success('Flatbuddy-backend is running')
+                setTestingFlatbuddyBackendResult(true)
+            }
+        } catch (error) {
+            toast.error('Flatbuddy-backend is not running')
+            setTestingFlatbuddyBackendResult(false)
+        }
+        setTestingFlatbuddyBackend(false)
+    }
+    return (
+        <div className="h-screen w-full flex  m-14">
+            <div className="m-10 flex flex-col items-start justify-start">
+                <div className="flex flex-row items-start justify-start w-full px-10 bg-slate-200 rounded-md p-4 shadow-xl">
+                    <Button variant="outline" disabled={testingFlatbuddyBackend} onClick={testFlatbuddyBackend}>
+                        Test Flatbuddy-backend
+                    </Button>
+                    <div className="flex flex-row items-center justify-center my-auto">
+                        {
+                            testingFlatbuddyBackend ? <Loader2 className="animate-spin size-4 ml-2" /> : testingFlatbuddyBackendResult ? <CheckCircle2 className="size-4 ml-2" /> : <X className="size-4 ml-2" />
+                        }
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default TestPage
